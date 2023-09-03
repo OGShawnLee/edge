c391d0e03ce5ee0f0f03226f2daf62049561bb6e
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-09-03 18:52:31 +0000

    feat: style sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,24 @@
   import "@unocss/reset/tailwind-compat.css";
 </script>
 
-<div class="min-h-screen bg-zinc-900 text-zinc-300 font-[inter]">
-  <slot />
-</div>
+<slot />
+
+<style>
+  :global(body) {
+    @apply h-screen bg-zinc-900 text-zinc-300 font-roboto;
+  }
+
+	:global(::-webkit-scrollbar) {
+		background-color: hsl(240, 8%, 14%);
+		width: 0.75vw;
+		min-width: 0.5rem;
+	}
+
+	:global(::-webkit-scrollbar-thumb) {
+		background-color: hsl(240, 12%, 30%);
+	}
+
+	:global(::-webkit-scrollbar-thumb:hover) {
+		background-color: hsl(240, 15%, 40%);
+	}
+</style>
