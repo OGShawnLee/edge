6e2e3d0275850a7950029601d41ae4d230a0d306
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-09-14 00:22:16 +0000

    fix(styling): ensure scrollbar is only shown when needed

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col gap-24px">
 	<TopHeader />
-	<div class="relative max-w-6xl w-full min-h-screen mx-auto | flex gap-32px">
+	<div class="relative max-w-6xl w-full mx-auto | flex gap-32px">
 		<Sidebar />
 		<Separator orientation="vertical" />
 		<main class="flex flex-col flex-[600px]">
