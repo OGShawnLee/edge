69f6634b856f2d0003c7f1916197285d500af479
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-09-24 03:56:46 +0000

    feat(ui): add dialog transitions

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -1,31 +1,40 @@
 <script lang="ts" context="module">
-  function hide_scrollbar() {
-    const initial_overflow = document.body.style.overflow;
-    document.body.style.overflow = "hidden"
-    return {
-      destroy: () => {
-        document.body.style.overflow = initial_overflow
-      }
-    }
-  }
+	function hide_scrollbar() {
+		const initial_overflow = document.body.style.overflow;
+		document.body.style.overflow = "hidden";
+		return {
+			destroy: () => {
+				document.body.style.overflow = initial_overflow;
+			}
+		};
+	}
 </script>
 
 <script lang="ts">
-	import { Dialog, DialogContent, DialogOverlay } from "malachite-ui";
+	import { fade, scale } from "svelte/transition";
+	import { Dialog } from "malachite-ui";
 
 	let initialFocus: HTMLElement | undefined = undefined;
-	
+
 	export { initialFocus as initial_focus };
-  export let open = false;
+	export let open = false;
 </script>
 
-<Dialog class="fixed inset-0 z-20 | grid place-content-center" {initialFocus} bind:open>
-	<DialogOverlay class="backdrop" />
-	<DialogContent
+<Dialog
+	class="fixed inset-0 z-20 | grid place-content-center"
+	{initialFocus}
+	bind:open
+	let:content
+	let:close
+	let:overlay
+>
+	<div class="backdrop" use:overlay transition:fade|global={{ duration: 200 }} />
+	<div
 		class="z-10 | w-lg pb-6 bg-dialog-color rounded-lg overflow-hidden"
-		use={[hide_scrollbar]}
-		let:close
+		use:hide_scrollbar
+		use:content
+		transition:scale|global={{ duration: 200, start: 1.15 }}
 	>
 		<slot {close} />
-	</DialogContent>
+	</div>
 </Dialog>
