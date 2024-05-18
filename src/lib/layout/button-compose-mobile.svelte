8641973961bb30508d5cbd35e3000ab3744cd3df
Shawn <changoloco.evg@gmail.com>
2024-05-18 18:26:36 +0000

    fix(ui): render mobile button compose if user is logged in

--- src/lib/layout/button-compose-mobile.svelte
+++ src/lib/layout/button-compose-mobile.svelte
@@ -1,17 +1,20 @@
 <script>
 	import { PenTool } from "lucide-svelte";
 	import { move_on_scroll_down } from "$lib/actions";
+	import { user } from '$lib/state';
 </script>
 
-<a
-	class="fixed bottom-84px -right-3 xl:hidden | w-16 h-16 | rounded-l-full | bg-teal-600 | flex items-center justify-center | shadow-lg"
-	aria-label="compose"
-	href="/i/compose"
-	use:move_on_scroll_down={{
-		offset: 36,
-		position: "bottom",
-		translate_class_name: "translate-y-16"
-	}}
->
-	<PenTool size={32} />
-</a>
+{#if $user}
+	<a
+		class="fixed bottom-84px -right-3 xl:hidden | w-16 h-16 | rounded-l-full | bg-teal-600 | flex items-center justify-center | shadow-lg"
+		aria-label="compose"
+		href="/i/compose"
+		use:move_on_scroll_down={{
+			offset: 36,
+			position: "bottom",
+			translate_class_name: "translate-y-16"
+		}}
+	>
+		<PenTool size={32} />
+	</a>
+{/if}
